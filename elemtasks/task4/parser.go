9c9319ffdbd38394b9bb22dc9bc844c9f3eeb778
art07 <artlock1318@gmail.com>
2021-07-09 08:52:12 +0300

    elemtasks task4 second commit

--- elemtasks/task4/parser.go
+++ elemtasks/task4/parser.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -13,25 +13,31 @@ var strForTask4 = "\ntask4 is a tool for jobs with files.\n" +
 	"Works in two modes. With two arguments, the number of occurrences; with three, line replacement.\n" +
 	"Mode1. Args:\n" +
 	"\targ1 pathToFile (С:/docs/test.txt);\n" +
-	"\targ2 search string (any string).\n" +
+	"\targ2 search string (\"any string\").\n" +
 	"Mode2. Args:\n" +
 	"\targ1 pathToFile (С:/docs/test.txt);\n" +
-	"\targ2 search string (any string);\n" +
-	"\targ3 change string (any string)."
+	"\targ2 search string (\"any string\");\n" +
+	"\targ3 change string (\"any string\")."
 
 //goland:noinspection GoPrintFunctions
 func main() {
 	switch len(args) {
 	case 2:
-		fmt.Println("Mode #1")
 		i, err := numberOfOccurrences()
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
+		fmt.Println("Mode #1")
 		fmt.Printf("[%s] meets %d time(s)\n", args[1], i)
 	case 3:
 		fmt.Println("Mode #2")
-		// do some job ...
+		err := changeText()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Mode #2 finished!")
 	default:
 		fmt.Println(strForTask4)
 	}
@@ -39,19 +45,22 @@ func main() {
 
 //goland:noinspection GoUnhandledErrorResult
 func numberOfOccurrences() (int, error) {
-	file, err := os.Open(args[0])
+	buf, err := ioutil.ReadFile(args[0])
 	if err != nil {
 		return 0, err
 	}
-	defer file.Close()
+	return strings.Count(string(buf), args[1]), nil
+}
 
-	snr := bufio.NewScanner(file)
-	times := 0
-	for snr.Scan() {
-		times += strings.Count(snr.Text(), args[1])
+//goland:noinspection GoUnhandledErrorResult
+func changeText() error {
+	buf, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		return err
 	}
-	if snr.Err() != nil {
-		return times, snr.Err()
+	str := strings.ReplaceAll(string(buf), args[1], args[2])
+	if err = ioutil.WriteFile(args[0], []byte(str), 0); err != nil {
+		return err
 	}
-	return times, nil
+	return nil
 }
