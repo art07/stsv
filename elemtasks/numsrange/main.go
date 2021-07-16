edb6c5c4d3f5d51213f8051f8405db6abf3d6867
art07 <artlock1318@gmail.com>
2021-07-16 15:37:34 +0300

    elemtasks task7 next commit

--- elemtasks/numsrange/main.go
+++ elemtasks/numsrange/main.go
@@ -1,35 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 )
 
 var strForTask7 = "\ntask7 is a tool to find numbers that in square less than given number.\n" +
 	"Min value => 1.\n" +
-	"Args:\n" +
-	"\targ1> number (150)."
+	"Flag:\n" +
+	"\tflag > -h help info (-h);\n" +
+	"\tflag > -n any number (-n=150)."
+
+var fHelp bool
+var fNumber int
 
 func main() {
-	if len(os.Args) < 2 {
+	/*Флажки.*/
+	flag.BoolVar(&fHelp, "h", false, "help info")
+	flag.IntVar(&fNumber, "n", 0, "input number")
+	flag.Parse()
+
+	if fHelp || fNumber < 1 {
 		fmt.Println(strForTask7)
 		return
 	}
 
-	number, err := strconv.Atoi(os.Args[1])
-	if err != nil || number < 1 {
-		fmt.Println(strForTask7)
-		return
-	}
 	var str string
 	for i := 1; ; i++ {
-		if (i * i) < number {
+		if (i * i) < fNumber {
 			str += fmt.Sprintf("%d,", i)
-		} else {
-			break
+			continue
 		}
+		break
 	}
 	fmt.Println(strings.TrimRight(str, ","))
 }
