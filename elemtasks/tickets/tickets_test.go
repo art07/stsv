87577e382f608c1023b90a88828c2f63d532e214
art07 <artlock1318@gmail.com>
2021-07-21 13:51:01 +0300

    elemtasks tickets test cover 72.4%

--- elemtasks/tickets/tickets_test.go
+++ elemtasks/tickets/tickets_test.go
@@ -12,6 +12,11 @@ type testData struct {
 	want string
 }
 
+type InOut struct {
+	in  string
+	in2 string
+}
+
 func TestPrepareAnswer(t *testing.T) {
 	tests := []testData{
 		{list: []string{}, want: "[ mode]. Lucky tickets in file = [0]\n"},
@@ -33,23 +38,54 @@ func TestGetTicketNumber(t *testing.T) {
 }
 
 func TestWriteFile(t *testing.T) {
-	err := writeFile("901361\n123456\n040150", "D:/IT/Golang/go/src/art/edu/stsv/elemtasks/tickets/tickets_test.txt")
-	// assert for nil
-	assert.Nil(t, err)
+	tests := [2]InOut{
+		{in: "901361\n123456\n040150", in2: "D:/IT/Golang/go/src/art/edu/stsv/elemtasks/tickets/tickets_test.txt"},
+	}
+
+	for _, test := range tests {
+		err := writeFile(test.in, test.in2)
+		if err != nil {
+			// assert for not nil
+			assert.NotNil(t, err)
+		} else {
+			// assert for nil
+			assert.Nil(t, err)
+		}
+	}
 }
 
 func TestGetTickets(t *testing.T) {
-	tickets, err := getTickets("D:/IT/Golang/go/src/art/edu/stsv/elemtasks/tickets/tickets_test.txt")
-	// assert equality
-	assert.Equal(t, []string{"901361", "123456", "040150"}, tickets, "they should be equal")
-	// assert for nil
-	assert.Nil(t, err)
+	tests := [2]InOut{
+		{in: "D:/IT/Golang/go/src/art/edu/stsv/elemtasks/tickets/tickets_test.txt"},
+		{in: "D:/IT/Golang/go/src/art/edu/stsv/elemtasks/tickets/tickets_test_123.txt"},
+	}
+
+	for _, test := range tests {
+		tickets, err := getTickets(test.in)
+		if err != nil {
+			assert.NotNil(t, err)
+			assert.Nil(t, tickets)
+		} else {
+			assert.Nil(t, err)
+			assert.NotNil(t, tickets)
+			assert.Equal(t, []string{"901361", "123456", "040150"}, tickets, "they should be equal")
+		}
+	}
 }
 
 func TestFillTickets(t *testing.T) {
-	err := fillTickets("D:\\IT\\Golang\\go\\src\\art\\edu\\stsv\\elemtasks\\tickets\\tickets.txt")
-	// assert for nil
-	assert.Nil(t, err)
+	tests := [2]InOut{
+		{in: "D:\\IT\\Golang\\go\\src\\art\\edu\\stsv\\elemtasks\\tickets\\tickets.txt"},
+	}
+
+	for _, test := range tests {
+		err := fillTickets(test.in)
+		if err != nil {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+	}
 }
 
 type testData2 struct {
@@ -68,12 +104,21 @@ func TestLuckyTickets(t *testing.T) {
 		{td: testData{list: []string{"123456", "234567", "345678"}}, mode: "Piter", wantSl: []string{}},
 		{td: testData{list: []string{"901361", "123456", "040150", "912660", "011000"}}, mode: "Moscow", wantSl: []string{"901361", "912660"}},
 		{td: testData{list: []string{"901361", "123456", "040150", "912660", "011000"}}, mode: "Piter", wantSl: []string{"040150", "011000"}},
+		{td: testData{list: []string{"123def"}}, mode: "Moscow", wantSl: []string{"040150", "011000"}},
+		{td: testData{list: []string{"abcdef"}}, mode: "Piter", wantSl: []string{"040150", "011000"}},
 	}
 
 	for _, test := range tests {
-		result, _ := checkLuckyTickets(test.td.list, test.mode)
-		// assert equality
-		assert.Equal(t, test.wantSl, result, "they should be equal")
+		result, err := checkLuckyTickets(test.td.list, test.mode)
+		if err != nil {
+			assert.NotNil(t, err)
+			assert.Nil(t, result)
+			assert.NotEqual(t, test.wantSl, result)
+		} else {
+			assert.Nil(t, err)
+			assert.NotNil(t, result)
+			assert.Equal(t, test.wantSl, result, "they should be equal")
+		}
 	}
 }
 
@@ -85,11 +130,17 @@ type testData3 struct {
 func TestGetSum(t *testing.T) {
 	tests := []testData3{
 		{"123", 6},
+		{"abc", 6},
 	}
 
 	for _, test := range tests {
-		result, _ := getSum(test.strNumber)
-		// assert equality
-		assert.Equal(t, test.want, result, "they should be equal")
+		result, err := getSum(test.strNumber)
+		if err != nil {
+			assert.NotNil(t, err)
+			assert.Equal(t, 0, result)
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, test.want, result, "they should be equal")
+		}
 	}
 }
